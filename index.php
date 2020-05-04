cbb4e4e601fbd992bcb0a90e60fdb363052ef517
Koronal <nikita062499@gmail.com>
2020-05-04 13:57:34 +0300

    переход

--- index.php
+++ index.php
@@ -7,8 +7,8 @@
 </head>
 <body class="background">
     <div class="authorization">
-
+        <h1 class="white h1-2">Авторизация</h1>
     </div>
-
+    <h1 class="white h1-1">Игра предназначена для широкоформатных мониторов</h1>
 </body>
 </html>

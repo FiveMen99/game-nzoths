57ed4a19cde58eefa7c7dea85b52c05d6991bbed
Koronal <nikita062499@gmail.com>
2020-05-07 00:51:54 +0300

    v1.0

--- js/games.js
+++ js/games.js
@@ -1,10 +1,30 @@
+<<<<<<< Updated upstream
+=======
+//Задание начального окна игры
+var cvs = document.getElementById("canvas");
+cvs.width= window.innerWidth;
+cvs.height = window.innerHeight;
+cvs.onkeydown = move;
+>>>>>>> Stashed changes
 
 var start_game = 1; //Переменная отвечающая за начало игры
 
+<<<<<<< Updated upstream
 if(start_game==1)
 $("#canvas").css('display', 'block');
+=======
+var ctx = cvs.getContext("2d");
+
+//Установка картинок и начальных координат для игрока
+var player = new Image();
+player.src ="img/sovukh.png";
+
+var player1 = new Image();
+player1.src ="img/sovukh1.png";
+>>>>>>> Stashed changes
 
 
+<<<<<<< Updated upstream
 var cvs = document.getElementById("canvas");
 var ctx = cvs.getContext("2d");
 cvs.width=window.innerWidth-20;
@@ -95,3 +115,62 @@ function draw(){
     }
 }
 player.onload =draw();
+=======
+var nzoths = new Image();
+nzoths.src ="img/nzoths.png";
+
+
+
+var backgroundImage = new Image();
+backgroundImage.src = "img/7.jpg";
+
+var x = 40;
+var y = 0;
+var xn = 1000;
+var yn = cvs.height-60;
+
+
+player.onload = draw();
+var a =0;
+function draw(){
+    ctx.clearRect(0, 0, ctx.width, ctx.height);
+    ctx.drawImage(backgroundImage,0,0);
+    if(a==0)
+        ctx.drawImage(player,x,y);
+    else
+    {
+        if(a<20)
+        {
+            ctx.drawImage(player1,x,y);
+        }
+        else
+            ctx.drawImage(player,x,y);
+        a++;
+    }
+
+    if(a==40)
+        a=0;
+
+    ctx.drawImage (floor,xn,yn);
+    ctx.drawImage (floor,xn-1014,yn);
+    ctx.drawImage (floor,xn+1014,yn);
+    ctx.drawImage (nzoths,cvs.width-400,cvs.height-700);
+    if (y < cvs.height-60 - player.height) y++;
+    xn=xn-5;
+    if(xn<-50) //условие проверки конца блока
+    {
+        xn=1000;
+    }
+        requestAnimationFrame(draw);
+}
+
+function move(e)
+{
+ y=y-10;
+ a=1;
+ console.log(e.keyCode);
+}
+
+document.addEventListener("keydown", move);
+
+>>>>>>> Stashed changes
